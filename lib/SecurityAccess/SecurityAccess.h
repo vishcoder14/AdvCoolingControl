578ca9b2285ea318e075c021be22303c75bdf3bb
vishcoder14 <vishnuskky2001@gmail.com>
2022-12-21 14:43:49 +0530

    fixed!

--- lib/SecurityAccess/SecurityAccess.h
+++ lib/SecurityAccess/SecurityAccess.h
@@ -12,7 +12,7 @@ void setup()
   Serial.begin(9600);   // Initiate a serial communication
   SPI.begin();      // Initiate  SPI bus
   mfrc522.PCD_Init();   // Initiate MFRC522
-  Serial.println("Approximate your card to the reader...");
+  Serial.println("please scan your card");
   Serial.println();
 
 }
